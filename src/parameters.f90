13491438b1e1037520d55630681f8058147ca3ef
Albert Katbashev <katbashev@thch.uni-bonn.de>
2023-05-06 22:28:16 +0200

    add the tuntyps
    Signed-off-by: Albert Katbashev <katbashev@thch.uni-bonn.de>

--- src/parameters.f90
+++ src/parameters.f90
@@ -3,6 +3,10 @@ module parameters
     public 
     !> Runtyps
     integer, parameter :: p_run_rhf = 0
+    integer, parameter :: p_run_uhf = 1
+    integer, parameter :: p_run_mp2 = 2
+    integer, parameter :: p_run_opt = 3
+    integer, parameter :: p_run_mpop = 4
 
     !> settings for calculation
     type type_set
