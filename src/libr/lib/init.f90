0ce4132ad35026002b77eca9bf35d86cbbe919ba
albert <92109627+Albkat@users.noreply.github.com>
2023-06-06 16:51:29 +0200

    updates for timing
    Signed-off-by: albert <92109627+Albkat@users.noreply.github.com>

--- src/libr/lib/init.f90
+++ src/libr/lib/init.f90
@@ -2,13 +2,20 @@ subroutine lib_init(prog,ntimer,verbose)
 
     use environment, only : init
     use global
+    use time
+
     implicit none
     character(len=*), intent(in) :: prog
     integer, intent(in) :: ntimer
     logical, intent(in) :: verbose
-    
-    allocate(persistentEnv)
+   
+    !> initilize the system time
+    call start_timing_run
+    call init_timing(ntimer,level=verbose)
+    call strat_timing(1)
 
+
+    allocate(persistentEnv)
     call init(persistentEnv)
 
 end subroutine
--- src/timing.f90
+++ src/timing.f90
@@ -1,14 +1,39 @@
 module time
     implicit none
 contains
-subroutine print_timestring(mode)
+function print_timestring(mode) result(str)
     implicit none
-    character(len=*), intent(in) :: mode
-        !! time point
     !> consts
-
+    character(len=*), parameter :: fmt= &
+    & '(a,''/'',a,''/'',a,1x,a'':'',a,'':'',a)'
+        !! output format
     
-    !> local vars
+    !> dummy arg list
+    character(len=*), intent(in) :: mode
+        !! time point 'S', 'E', or other
+    character(len=31), intent(out) :: str
+        !! time string
 
-endsubroutine 
+    !> local vars
+    character(len=8) :: date
+    character(len=10) :: time
+    character(len=5) :: zone
+    
+    select case(mode)
+    case('S','s')
+        write(str,fmt) &
+        &   start_date(:4), start_date(5:6), start_date(7:), &
+        &   start_time(:2), start_time(3:4), start_time(5:)
+    case ('E','e')
+        write(str,fmt) &
+        &   stop_date(:4), stop_date(5:6), stop_date(7:), &
+        &   stop_time(:2), stop_time(3:4), stop_time(5:)
+    case default
+        call date_time(date,time,zone)
+        write(str,fmt) &
+        &   date(:4), date(5:6), date(7:), &
+        &   time(:2), time(3:4), time(5:)
+    endselect
+
+end function print_timestring 
 end module time
