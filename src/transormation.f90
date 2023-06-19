1ff4009b2e1384441be4912126ede684dfe8ce73
albert <92109627+Albkat@users.noreply.github.com>
2023-06-19 17:49:42 +0200

    add transfer from number o symbols of PSE
    Signed-off-by: albert <92109627+Albkat@users.noreply.github.com>

---
+++ src/transormation.f90
@@ -0,0 +1,58 @@
+module transformation
+    implicit none
+    public :: toSymbol
+    private
+    
+    !> Periodic system of elements
+    character(len=2), parameter :: pse(118) = [ &
+    & 'H ','He', &
+    & 'Li','Be','B ','C ','N ','O ','F ','Ne', &
+    & 'Na','Mg','Al','Si','P ','S ','Cl','Ar', &
+    & 'K ','Ca', &
+    & 'Sc','Ti','V ','Cr','Mn','Fe','Co','Ni','Cu','Zn', &
+    &           'Ga','Ge','As','Se','Br','Kr', &
+    & 'Rb','Sr', &
+    & 'Y ','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd', &
+    &           'In','Sn','Sb','Te','I ','Xe', &
+    & 'Cs','Ba', &
+    & 'La','Ce','Pr','Nd','Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb', &
+    & 'Lu','Hf','Ta','W ','Re','Os','Ir','Pt','Au','Hg', &
+    &           'Tl','Pb','Bi','Po','At','Rn', &
+    & 'Fr','Ra', &
+    & 'Ac','Th','Pa','U ','Np','Pu','Am','Cm','Bk','Cf','Es','Fm','Md','No', &
+    & 'Lr','Rf','Db','Sg','Bh','Hs','Mt','Ds','Rg','Cn', &
+    &           'Nh','Fl','Mc','Lv','Ts','Og' ]
+
+contains
+
+!-------------------------------------------------
+!> Just a wrapper for numberToSymbol
+!-------------------------------------------------
+elemental function toSymbol(number) result(symbol)
+
+    integer, intent(in) :: number
+    character(len=2) :: symbol
+
+    call numberToSymbol(symbol,number)
+    
+end function toSymbol
+
+!-------------------------------------------------
+!> from number to periodic system of elements 
+!-------------------------------------------------
+elemental subroutine numberToSymbol(symbol,number)
+
+    character(len=2), intent(out) :: symbol
+        !! element symbol
+    integer, intent(in) :: number
+        !! atomic number
+
+    if (number <= 0 .or. number > size(pse)) then
+        symbol = '--'
+    else
+        symbol = pse(number)
+    endif
+
+end subroutine numberToSymbol
+
+end module transformation
--- src/type/mol.f90
+++ src/type/mol.f90
@@ -4,8 +4,9 @@
 
 module molecule
     use iso_fortran_env, only : wp => real64
+    use transformation, only : toSymbol
     implicit none
-    public :: type_molecule
+    public :: type_molecule, init
 
     private
 
@@ -27,5 +28,33 @@ module molecule
         real(wp) :: charge = 0.0_wp
     
     end type type_molecule
+
+    interface init
+        module procedure :: initMoleculeNumbers ! 1 station
+        !module procedure :: initMoleculeSymbols ! 2 station
+    endinterface init
+contains
+
+subroutine initMoleculeNumbers(mol, at, xyz)
+    
+    type(type_molecule), intent(out) :: mol 
+        !! instance of mol structure -> result
+    integer, intent(in) :: at(:)
+        !! atomic numbers
+    real(wp), intent(in) :: xyz(:,:)
+        !! cartesian coordinates
+
+    character(len=4), allocatable :: sym(:)
+        !! symbols
+    integer :: nAt
+
+    nAt = min(size(at, dim=1), size(xyz, dim=2))
+        !! choose the minimal number of elements 
+    allocate(sym(nAt))
+    sym(:) = toSymbol(at(:nAt))
+
+    !call init(mol,at,sym,xyz)
+
+end subroutine initMoleculeNumbers
 
 end module molecule
